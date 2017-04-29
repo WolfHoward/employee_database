3688a6147da35e4472c95b7bda511d3f8d8f865e
WolfHoward <wolfahoward@outlook.com>
2017-04-29 15:39:50 -0500

    Added views to home and auth blueprints.

---
+++ app/auth/views.py
@@ -0,0 +1,24 @@
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+
+from . import auth
+from forms import LoginForm, RegistrationForm
+from .. import db
+from ..models import Employee
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    Handle requests to the /register route
+    Add an employee to the database through the registration form
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        employee = Employee(email=form.email.data,
+                            username=form.username.data,
+                            first_name=form.first_name.data,
+                            last_name=form.last_name.data,
+                            password=form.password.data)
+
+        # dd employee to the database
+        db.session.add(employee)
